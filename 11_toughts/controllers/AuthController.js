4f52ae9d2f8a3829eb822c58765ea619c47fce3f
murilo-russo-r2sis <148149459+murilo-russo-r2sis@users.noreply.github.com>
2024-08-27 11:24:33 -0300

    Criado sistema de registro

--- 11_toughts/controllers/AuthController.js
+++ 11_toughts/controllers/AuthController.js
@@ -19,5 +19,40 @@ module.exports = class AuthController {
             res.render('auth/register');
             return;
         }
+
+        // check if user exists
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+        if(checkIfUserExists){
+            req.flash('message', 'O e-mail já está em uso.');
+            res.render('auth/register');
+            return;
+        }
+        //create password
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try {
+            const createdUser = await User.create(user);
+
+            // initialize session
+
+            req.session.userid = createdUser.id;
+
+            req.flash('message', 'Cadastro realizado com sucesso1');
+
+            req.session.save(()=>{
+                res.redirect('/');
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+
     }
 }
