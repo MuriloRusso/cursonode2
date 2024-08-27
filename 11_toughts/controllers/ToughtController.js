d8662087b99154e453593635615d41018d5fb15f
murilo-russo-r2sis <148149459+murilo-russo-r2sis@users.noreply.github.com>
2024-08-27 15:53:21 -0300

    wip

--- 11_toughts/controllers/ToughtController.js
+++ 11_toughts/controllers/ToughtController.js
@@ -4,6 +4,31 @@ const User = require('../models/User');
 
 module.exports = class ToughtController {
     static async showToughts(req, res){
-        res.render('toughts/home')
+        const toughts = await Tought.findAll();
+        // const users = await User.findAll();
+        // console.log(users);
+
+        /*const toughts = await Tought.findAll({
+            include: [{
+              model: User,
+              attributes: ['name'],
+              required: true // Faz um inner join
+            }]
+          });*/
+        
+        //   console.log(toughts);
+        res.render('toughts/home', {toughts: toughts/*, users: users*/});
+    }
+
+    static async dashboard(req, res){
+        console.log(req.session);
+        
+        if(!req.session.userid){
+            req.flash('message', 'Você não está logado!');
+            res.redirect('/');
+            return;
+        }
+        const toughts = await Tought.findAll({where: {UserId: req.session.userid}});
+        res.render('toughts/dashboard', {toughts: toughts});
     }
 }
--- 11_toughts/routes/toughtsRoutes.js
+++ 11_toughts/routes/toughtsRoutes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 const ToughtController = require('../controllers/ToughtController')
 
-
-router.get('/', ToughtController.showToughts)
+router.get('/', ToughtController.showToughts);
+// router.get('/dashboard/:id', ToughtController.dashboard);
+router.get('/dashboard', ToughtController.dashboard);
 
 module.exports = router;
